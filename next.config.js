c20c38f6f926a241a09a5e698fae2754d2464cd9
ebenotopsoncrown <134868921+ebenotopsoncrown@users.noreply.github.com>
2025-08-29 23:07:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-}
-module.exports = nextConfig
- 
+  swcMinify: true,
+  productionBrowserSourceMaps: true, // <-- add this
+};
+
+module.exports = nextConfig;
